956824d1eda726c5da271bfad1f804b5da89c97d
Md Rahim Rahman <onlyrahimsec@gmail.com>
2026-08-12 16:05:41 +0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,9 @@
 import os
-
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "secureflow-development-secret-key"
+)
